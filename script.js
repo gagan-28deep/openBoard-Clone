6eb8be13622f5a9be38e5b660f6a5adea65de396
gagan-28deep <gagan_28_deep@outlook.com>
2022-05-20 13:36:07 +0530

    pencilFunctionalityAdded

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-let optionCont = document.querySelector('.options-cont')
-let optionFlag = true
+let toolsCont = document.querySelector(".tools-cont");
+let optionCont = document.querySelector(".options-cont");
+let optionFlag = true;
+let pencilToolCont = document.querySelector(".pencil-tool-cont");
+let eraserToolCont = document.querySelector(".eraser-tool-cont");
 
+let pencil = document.querySelector('.pencil')
+let eraser = document.querySelector('.eraser')
+
+let pencilFlag = false;
+let eraserFlag = false;
 // True -> tools show , false -> tools hide
 
-optionCont.addEventListener("click" , (e)=>{
-    optionFlag = !optionFlag
-    if(optionFlag) openTools()
-    else closeTools()
-})
+optionCont.addEventListener("click", (e) => {
+  optionFlag = !optionFlag;
+  if (optionFlag) openTools();
+  else closeTools();
+});
 
-function openTools(){
-    let iconElem = optionCont.children[0]
-    iconElem.classList.remove('fa-times')
-    iconElem.classList.add('fa-bars')
+function openTools() {
+  let iconElem = optionCont.children[0];
+  iconElem.classList.remove("fa-times");
+  iconElem.classList.add("fa-bars");
+  toolsCont.style.display = "flex";
 }
 
-function closeTools(){
-    let iconElem = optionCont.children[0]
-    iconElem.classList.remove('fa-bars')
-    iconElem.classList.add('fa-times')
+function closeTools() {
+  let iconElem = optionCont.children[0];
+  iconElem.classList.remove("fa-bars");
+  iconElem.classList.add("fa-times");
+  toolsCont.style.display = "none";
+  pencilToolCont.style.display = "none";
+  eraserToolCont.style.display = "none";
 }
+
+pencil.addEventListener("click", (e) => {
+  // true -> show pencil tools
+  // False => hide pencil tools
+  pencilFlag = !pencilFlag;
+
+  if (pencilFlag) {
+    pencilToolCont.style.display = "block";
+  } else {
+    pencilToolCont.style.display = "none";
+  }
+});
+
+eraser.addEventListener("click", (e) => {
+  // true -> show eraser tools
+  // False => hide eraser tools
+  eraserFlag = !eraserFlag;
+
+  if (eraserFlag) eraserToolCont.style.display = "flex";
+  else eraserToolCont.style.display = "none";
+});
