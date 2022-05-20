8003d99749bf19457a90bc46e5c337e5a5c60bd6
gagan-28deep <gagan_28_deep@outlook.com>
2022-05-20 16:01:16 +0530

    toolsFunctionalityAdded

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ let eraser = document.querySelector(".eraser");
 
 let sticky = document.querySelector(".sticky");
 
+let upload = document.querySelector(".upload");
+
 let pencilFlag = false;
 let eraserFlag = false;
 // True -> tools show , false -> tools hide
@@ -56,18 +58,104 @@ eraser.addEventListener("click", (e) => {
   else eraserToolCont.style.display = "none";
 });
 
+upload.addEventListener("click", (e) => {
+  // Open File Explorer
+  let input = document.createElement("input");
+  input.setAttribute("type", "file");
+  input.click();
+
+  input.addEventListener("change", (e) => {
+    let file = input.files[0]; // array type of files , we will get the first file we select.
+    let url = URL.createObjectURL(file);
+
+    let stickyTemplateHtml = `
+    <div class="header-cont">
+        <div class="minimize"></div>
+        <div class="remove"></div>
+    </div>
+    <div class="note-cont">
+        <img src="${url}"/>
+    </div>
+    `;
+
+    createSticky(stickyTemplateHtml)
+  })
+});
+
 sticky.addEventListener("click", (e) => {
-  let stickyCont = document.createElement("div");
-  stickyCont.setAttribute("class", "sticky-cont");
-  stickyCont.innerHTML = `
+  
+  let stickyTemplateHtml = `
     <div class="header-cont">
     <div class="minimize"></div>
     <div class="remove"></div>
     </div>
     <div class="note-cont">
-        <textarea></textarea>
+        <textarea spellcheck="false"></textarea>
     </div>
     `;
-
-    document.body.appendChild(stickyCont)
+    createSticky(stickyTemplateHtml)
 });
+
+
+function createSticky(stickyTemplateHtml){
+    let stickyCont = document.createElement("div");
+    stickyCont.setAttribute("class", "sticky-cont");
+    stickyCont.innerHTML = stickyTemplateHtml
+    document.body.appendChild(stickyCont);
+  
+    let minimize = stickyCont.querySelector(".minimize");
+    let remove = stickyCont.querySelector(".remove");
+  
+    noteActions(minimize, remove, stickyCont);
+  
+    stickyCont.onmousedown = function (event) {
+      dragAndDrop(stickyCont, event);
+    };
+  
+    stickyCont.ondragstart = function () {
+      return false;
+    };   
+}
+
+
+function noteActions(minimize, remove, stickyCont) {
+  remove.addEventListener("click", (e) => {
+    stickyCont.remove();
+  });
+  minimize.addEventListener("click", (e) => {
+    let noteCont = stickyCont.querySelector(".note-cont");
+    let display = getComputedStyle(noteCont).getPropertyValue("display");
+    if (display === "none") noteCont.style.display = "block";
+    else noteCont.style.display = "none";
+  });
+}
+
+function dragAndDrop(element, event) {
+  let shiftX = event.clientX - element.getBoundingClientRect().left;
+  let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+  element.style.position = "absolute";
+  element.style.zIndex = 1000;
+
+  moveAt(event.pageX, event.pageY);
+
+  // moves the element at (pageX, pageY) coordinates
+  // taking initial shifts into account
+  function moveAt(pageX, pageY) {
+    element.style.left = pageX - shiftX + "px";
+    element.style.top = pageY - shiftY + "px";
+  }
+
+  function onMouseMove(event) {
+    moveAt(event.pageX, event.pageY);
+  }
+
+  // move the element on mousemove
+  document.addEventListener("mousemove", onMouseMove);
+
+  // drop the element, remove unneeded handlers
+  element.onmouseup = function () {
+    document.removeEventListener("mousemove", onMouseMove);
+    element.onmouseup = null;
+  };
+}
